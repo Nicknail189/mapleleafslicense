1749804417f63a690e8ba8b2ecdbb9829bae2bec
nick nail <nicknail189@gmail.com>
2023-04-17 09:01:45 -0500

    edit

--- Login-page/pages/dashboard.vue
+++ Login-page/pages/dashboard.vue
@@ -50,12 +50,6 @@
 
 <script>
 
-// definePageMeta({
-//   layout: "footerLayout",
-// });
-// const layout = "footerLayout";
-
-
 export default {
     methods: {
       dashboard(){
@@ -66,16 +60,4 @@ export default {
       },
     }
 }
-</script>
-
-<!-- Smartsupp Live Chat script -->
-<script type="text/javascript">
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = '7463184a09ab54ab0f68789ec7f4b28ee055d916';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
 </script>
